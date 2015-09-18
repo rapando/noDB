9aea2bec43fa2128034e6a9c26087c0104f4f960
Samson Rapando <samson@rapando.co.ke>
2015-09-18 17:52:05 +0300

    Working part: fetching the usertypes on update.html. An alert will show if you are offline I alson deleted the angular.js library, the project will run on jquery on the client side

--- js/update.js
+++ js/update.js
@@ -1,1 +1,27 @@
-update.js
+$(document).ready(function() {
+	var domainlink = 'http://localhost/noDB';
+	getusertypes();
+
+
+	function getusertypes() {
+		$.ajax({
+			dataType:'jsonp',
+			jsonp:'cb',
+			url:domainlink+'/php/send.php?request=get_user_types',
+			timeout:10000,
+			success:function(data) {
+				data = JSON.parse(data);
+				var output = '';
+				for(var i = 0; i < data.length; i++) {
+					output += '<option value="'+data[i].id+'">'+data[i].type+'</option>';
+				}
+				var usertype = '<select id="usertype"><option value=""></option>'+output+'</select>';
+				var new_usertype = '<select id="new_usertype"><option value=""></option>'+output+'</select>';
+				$("#usertype").html(usertype);
+				$("#new_usertype").html(new_usertype);
+			}, error:function() {
+				alert("no internet");
+			}
+		});
+	}
+});

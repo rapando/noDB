0b3a8d737a5475eda572c4a12738e5dee6ea4e3f
Samson Rapando <samson@rapando.co.ke>
2015-09-19 19:23:17 +0300

    Change of plans

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
-main.js
+var app = angular.module('mainapp', []);
+app.controller('commentController', ['$scope', function($scope) {
+	$scope.comment = '';
+	$scope.rem = function() {
+		return 100 - $scope.comment.length;
+	};
+}]);
+
+// The JQUery Code
+var jq = $.noConflict();
+jq(document).ready(function() {
+	var domainlink = 'http://localhost/noDB';
+	jq("#loader").hide();
+	jq("#submit").on('click', function() {
+		jq("#loader").show();
+		var name = jq("#username").val();
+		name = name.trim();
+		var comment = jq("#newcomment").val();
+		comment = comment.trim();
+		if(name.length >= 2 && comment.length >=2) {
+			var link = domainlink + '/php/comment.php?req=1&name='+name+'&comment='+comment;
+			jq.ajax({
+				dataType:'jsonp',
+				jsonp:'cb',
+				timeout:10000,
+				success:function(data) {
+					alert(data);
+					jq("#loader").hide(500);
+				}, error:function() {
+					alert("check connection status");
+					jq("#loader").hide(500);
+				}
+			});
+
+			jq("#username").val('');
+			jq("#newcomment").val('');
+		} else {
+			alert("Name or comment too short");
+		}
+	});	
+});
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
-This is a system where the information is stored online, in JSON formats in .json files. The folders are created with PHP with custom privileges. 
+This is a system where the information is stored online, in JSON formats in .json files. The files will be created with PHP if they dont exist. The system holds the views of users on a certain topic managed by an admin.
 -----
 The tables are not related as in a relational database. However, the information can be related using indexes in the array. In any case an element in array is deleted, then the indexes are rearranged, then the unique user IDs (which are auto incremented) are used. 
 ----
 We will be using the local server environment for the PHP code. the url should be http://localhost/noDb
+______

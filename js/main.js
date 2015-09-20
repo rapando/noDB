5464f83040158bf207ae7e77ed7b6830d26f25b9
Samson Rapando <samson@rapando.co.ke>
2015-09-20 12:41:01 +0300

    Working prototype

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@ app.controller('commentController', ['$scope', function($scope) {
 	};
 }]);
 
+
 // The JQUery Code
 var jq = $.noConflict();
+var domainlink = 'http://localhost';
 jq(document).ready(function() {
-	var domainlink = 'http://localhost/noDB';
+	fetchcomments();
 	jq("#loader").hide();
 	jq("#submit").on('click', function() {
 		jq("#loader").show();
@@ -18,18 +20,19 @@ jq(document).ready(function() {
 		var comment = jq("#newcomment").val();
 		comment = comment.trim();
 		if(name.length >= 2 && comment.length >=2) {
-			var link = domainlink + '/php/comment.php?req=1&name='+name+'&comment='+comment;
 			jq.ajax({
 				dataType:'jsonp',
 				jsonp:'cb',
-				timeout:10000,
+				url:domainlink+'/nodb/php/comments.php?req=1&name='+name+'&comment='+comment,
+				
 				success:function(data) {
 					alert(data);
 					jq("#loader").hide(500);
+					fetchcomments();
 				}, error:function() {
-					alert("check connection status");
-					jq("#loader").hide(500);
-				}
+					alert("error posting comment");
+					
+				}, timeout:10000
 			});
 
 			jq("#username").val('');
@@ -38,4 +41,84 @@ jq(document).ready(function() {
 			alert("Name or comment too short");
 		}
 	});	
+
+	function fetchcomments() {
+		jq.ajax({
+			dataType:'jsonp',
+			jsonp:'cb',
+			url:domainlink+'/nodb/php/comments.php?req=2',
+			timeout:10000,
+			success:function(data) {
+				jq("#noc").html(data.length);
+				data=data.reverse();
+				var comments = '<ul>';
+				for(var i =0; i < data.length; i++) {
+					comments += '<li><p><b>'+data[i].name+'</b><br />'+data[i].comment+'<br /><em>'+showtime(data[i].moment)+'</em></p></li>';
+				}
+				comments += '</ul>';
+				jq("#comments").html(comments);
+			}, error:function() {
+				alert("No connection");
+			}
+		});
+	}
+
+	function showtime(epoch) {
+		epoch = epoch * 1000;
+		var now = new Date(epoch);
+		var x = now.getHours()+':'+now.getMinutes()+'  '+now.getDate()+'/'+gettruemonth(now.getMonth())+'/'+now.getFullYear();
+		return x;
+	}
+
+	function gettruemonth(x) {
+		switch(x) {
+			case 0:
+			return 'Jan';
+			break;
+
+			case 1:
+			return 'Feb';
+			break;
+
+			case 2:
+			return 'Mar';
+			break;
+
+			case 3:
+			return 'Apr';
+			break;
+
+			case 4:
+			return 'May';
+			break;
+
+			case 5:
+			return 'Jun';
+			break;
+
+			case 6:
+			return 'Jul';
+			break;
+
+			case 7:
+			return 'Aug';
+			break;
+
+			case 8:
+			return 'Sep';
+			break;
+
+			case 9:
+			return 'Oct';
+			break;
+
+			case 10:
+			return 'Nov';
+			break;
+
+			case 11:
+			return 'Dec';
+			break;
+		}
+	}
 });
